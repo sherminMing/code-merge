146237f1c51f30c1276a1eb346e26d6c6b681262
Smart Xia <smart.xia@sscinc.com>
2024-07-11 14:39:44 +0800

    Kick off of this project

---
+++ src/main/java/com/example/codeMerge/CodeMergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeMerge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMergeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMergeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeMerge/CodeMergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeMerge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
